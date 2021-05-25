d5532c1f5de02f70f5bf1570b4517325526effb8
pramod0302 <pramod0302@gmail.com>
2021-05-25 20:33:34 +0530

    Fixed the indentation problem and removed mistakes

--- applypage.php
+++ applypage.php
@@ -1,146 +1,70 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-.button {
-  background-color: #4CAF50;
-  border: none;
-  color: white;
-  padding: 10px 30px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 2px 2px;
-  cursor: pointer;
-}
-</style>
-<div id="IT company" class="tabcontent">
-  <form name='myform' action='homepage.php'>
+  <title>Apply page</title>
 </head>
-<body>
-
-<button class="HOME">HOME</button>
-
 </body>
-<form name='myform' action='homepage.php'>
-    <div class="jumbotron">
-</html>
+    <form name='myform' action='homepage.php'>
+      <button class="HOME">HOME</button>
+    </form>
+    <form name='myform' action='successpage.php'>
+      <center>
+        <h1> REGISTRATION FORM</h1> </center>
+        <hr>
+        <label> Firstname </label> 
+        <input type="text" name="firstname" placeholder= "Firstname" size="15"  /> 
+        <label> DOB: </label> 
+        <input type="text" name="DOB" placeholder="DOB" size="10"  /> 
+        <label> Lastname: </label>  
+        <input type="text" name="lastname" placeholder="Lastname" size="15" /> 
+        <button class='button' type='submit'>SUBMIT</button>
+      </center>
 
-<!-- Comment -->
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<style>
-body{
-  font-family: Calibri, Helvetica, sans-serif;
-  background-color: lightblue;
-}
-.container {
-    padding: 50px;
-  background-color: pink;
-}
- 
-input[type=text], input[type=password], textarea {
-  width: 100%;
-  padding: 15px;
-  margin: 5px 0 22px 0;
-  display: inline-block;
-  border: none;
-  background: #f1f1f1;
-}
-input[type=text]:focus, input[type=password]:focus {
-  background-color: orange;
-  outline: none;
-}
- div {
-      padding: 10px 0;
-      }
-hr {
-  border: 1px solid #f1f1f1;
-  margin-bottom: 25px;
-}
-.registerbtn {
-  background-color: #dcdcdc;
-  color: white;
-  padding: 16px 20px;
-  margin: 8px 0;
-  border: none;
-  cursor: pointer;
-  width: 100%;
-  opacity: 0.9;
-}
-.registerbtn:hover {
-  opacity: 1;
-}
-</style>
-<form name='myform' action='homepage.php'>
-</head>
-<body>
-  <center>
-      <h1> REGISTRATION FORM</h1> </center>
-      <hr>
-      <label> Firstname </label> 
-      <input type="text" name="firstname" placeholder= "Firstname" size="15" required /> 
-      <label> DOB: </label> 
-      <input type="text" name="DOB" placeholder="DOB" size="10" required /> 
-      <label> Lastname: </label>  
-      <input type="text" name="lastname" placeholder="Lastname" size="15"required /> 
-    <div>
-<label> 
-Course :
-</label>
+      <label> 
+      Course :
+      </label>
 
-Education :
-<textarea cols="50" rows="4" placeholder="Education" value="Education" required>
-</textarea>
-<select>
-  <option value="Course">Course</option>
-  <option value="BCA">BCA</option>
-  <option value="BBA">BBA</option>
-  <option value="B.Tech">B.Tech</option>
-  <option value="MBA">MBA</option>
-  <option value="MCA">MCA</option>
-  <option value="M.Tech">M.Tech</option>
-</select><br>
-<label> experince: </label>  
-      <input type="text" name="experince" placeholder="experince" size="15"required /> 
-<select>
-  <option value="expected salary">expected salary</option>
-   <option value="maximum">max</option>
-  <option value="minimum">min</option>
-  
-</select>
-</div>
-<div>
-<label> 
-Gender :
-</label><br>
-<input type="radio" value="Male" name="gender" checked > Male 
-<input type="radio" value="Female" name="gender"> Female 
-<input type="radio" value="Other" name="gender"> Other<br>
+      Education :
+      <textarea cols="50" rows="4" placeholder="Education" value="Education" >
+      </textarea>
+      <select>
+        <option value="Course">Course</option>
+        <option value="BCA">BCA</option>
+        <option value="BBA">BBA</option>
+        <option value="B.Tech">B.Tech</option>
+        <option value="MBA">MBA</option>
+        <option value="MCA">MCA</option>
+        <option value="M.Tech">M.Tech</option>
+      </select><br>
+      <label> experince: </label>  
+            <input type="text" name="experince" placeholder="experince" size="15" /> 
+      <select>
+        <option value="expected salary">expected salary</option>
+         <option value="maximum">max</option>
+        <option value="minimum">min</option>
+        
+      </select>
+      <div>
+      <label> 
+      Gender :
+      </label><br>
+      <input type="radio" value="Male" name="gender" checked > Male 
+      <input type="radio" value="Female" name="gender"> Female 
+      <input type="radio" value="Other" name="gender"> Other<br>
 
-</div>
-</label> 
-Phone :
-</label>
-<input type="text" name="country code" placeholder="Country Code"  value="+91" size="2"/> 
-<input type="text" name="phone" placeholder="phone no." size="10"/ required> 
-Current Address :
-<textarea cols="80" rows="5" placeholder="Current Address" value="address" required>
-</textarea>
+      </div>
+      </label> 
+      Phone :
+      <input type="text" name="country code" placeholder="Country Code"  value="+91" size="2"/> 
+      <input type="text" name="phone" placeholder="phone no." size="10"/ > 
+      Current Address :
+      <textarea cols="80" rows="5" placeholder="Current Address" value="address" >
+      </textarea>
 
-<!DOCTYPE html>
-<html>
-<body>
-<div>
-  <form name='myform' action='successpage.php'>
-    <!-- <button type="button" onclick="alert">submit!</button> -->
-    <button class='button' type='submit'>SUBMIT</button>
-  </form>
-</div>
- 
-</body>
+        <!-- <button type="button" onclick="alert">submit!</button> -->
+      <button class='button' type='submit'>SUBMIT</button>
+    </form>
+   
+  </body>
 </html>
 
