117fbe2fac4431870f79c16699167fd6c398cfeb
pramod0302 <pramod0302@gmail.com>
2021-05-30 13:22:41 +0530

    Added the submit page and names to be sent when you hit apply button

---
+++ submitpage.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Submit page</title>
+  <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script type="text/javascript" src="homepage.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+</head>
+<body>
+	<center>
+		<h1 class="display-2">Your application is submitted successfully</h1>
+		<form name='myform' action='homepage.php'>
+			<button class="btn btn-outline-success">GO TO HOMEPAGE</button>
+		</form>
+	</center>
+</body>
+</html>
