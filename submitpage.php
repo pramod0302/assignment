496fface78aaf499287300f7fd60619cdb8b70c8
pramod0302 <pramod0302@gmail.com>
2021-06-05 12:59:22 +0530

    Added provision to save the apply page in a databas and new file for storing sql commanfs info

--- submitpage.php
+++ submitpage.php
@@ -35,69 +35,29 @@
 			}
 			// echo "Connected successfully\n '<br>'";
 			$query = "SELECT * FROM `employeet`";
-	        $my_var = 0;
 
+			// echo "Connected successfully\n '<br>'";
+			$query = "SELECT * FROM `employeet`";
+	        $name = $_GET['name'];
+	        $email = $_GET['email'];
+	        $address = $_GET['address'];
+	        $qualification = $_GET['qualification'];
+	        $expyears = $_GET['expyears'];
+	        $expprev = $_GET['exp'];
+			echo "HELLO WORLD";
+			$query = "INSERT INTO `employeet`(`address`, `email`, `expprev`, `expyears`, `name`, `qualification`) VALUES ('".$address."',
+							'".$email."', '".$expprev."', '".$expyears."', '".$name."', '".$qualification."')";
+			$my_var = 0;
 			if ($is_query_run = mysqli_query($conn,$query)) 
 			{ 
-			    // echo "Query Executed"; 
-			    // loop will iterate until all data is fetched 
-
-			    while ($query_executed = mysqli_fetch_assoc ($is_query_run)) 
-			    { 
-			        // these four line is for four column 
-			        $picture = 'idli.jpg';
-
-			        echo "<tr>";
-					echo "<td>" . $query_executed['name']. "</td>";
-					echo "<td id='test' height=100 width=100>" . $query_executed['email']. " </td>";
-					echo "<td id='test' height=100 width=100>" . $query_executed['address']. " </td>";
-					// echo "<td>" . $query_executed['Item Picture']. "</td>";
-					// echo $picture;
-					// echo "<td> <img src=".$query_executed['address']." alt='' border=3 height=100 width=200></img> </td>";
-					//echo "<td> <input name=".$query_executed['Item Name']." id=".$query_executed['Item Name']." type=number min=0 max=10 value=0></td>";
-					// echo "<td>" . $query_executed['info']. "</td>";
-		  	// 		echo 	"<td> <input name=".$query_executed['Item Name']." type='text' id=".$query_executed['Item Name']." type=number min=0 max=10 value=0></td>";
-					// echo "</tr>";
-					// $my_var += 1;
-					// if($my_var == 5)
-					// {
-					// 	echo "<tr>
-	    //         			<th class='wrapper' colspan='5'>Main Course</th> 
-	    //     			</tr>";
-					// }
-					// if($my_var == 10) {
-					// 	echo "<tr>
-	    //         			<th class='wrapper' colspan='5'>Juice</th> 
-	    //     			</tr>";	
-					// }
-			    } 
-				echo "</table>";
-				$var = 'dosa';
-				// if(isset($_POST[$query_executed['Item Name']])){
-				// 	echo $_POST['dosa'];
-				// }
-				// if(isset($_POST['dosa'])){
-				// 	echo $_POST['dosa'];
-				// }
-				// echo "<button class='button' type='submit' onclick=window.open('admin.php','_blank') name='place' >PLACE ORDER</button>";
-				// echo "<div class='wrapper'>
-				// 	<button class='button' type='submit' name='place' >PLACE ORDER</button> 
-				// 	</div>";
-
-				// $varName = $_GET['name'];
-				// $varTable = $_GET['email'];
-				// $varOrderType = $_GET['address'];
-				// echo "<input type='hidden' name='name' value='".$varName."' />";
-				// echo "<input type='hidden' name='tablenumber' value='".$varTable."' />";				
-				// echo "<input type='hidden' name='orderType' value='".$varOrderType."' />";				
-				// echo "</form>";
-				// $value = $_POST['dosa'];
-				// echo $value;
-			} 
-			else
-			{ 
-			    echo "Error in execution"; 
-			} 
+				// while ($query_executed = mysqli_fetch_assoc ($is_query_run)) 
+			//    {
+				// 	echo $query_executed['ord'];
+			//    } 
+			}
+			else {
+				echo "Error in execution";
+			}
 			mysqli_close($conn);
 	?>
 </body>
